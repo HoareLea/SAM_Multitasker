1b38d3f18b3661cdbc1f3a716457fb62098a8405
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-02-20 11:54:21 +0100

    ActiveSetting loading bug fixed

--- SAM_Multitasker/SAM.Core.Multitasker/Manager/ActiveSetting.cs
+++ SAM_Multitasker/SAM.Core.Multitasker/Manager/ActiveSetting.cs
@@ -4,13 +4,15 @@ namespace SAM.Core.Multitasker
 {
     public static partial class ActiveSetting
     {
-        private static Setting setting = Load();
+        private static Setting setting = null;
 
         private static Setting Load()
         {
             Setting setting = ActiveManager.GetSetting(Assembly.GetExecutingAssembly());
             if (setting == null)
+            {
                 setting = GetDefault();
+            }
 
             return setting;
         }
@@ -19,6 +21,11 @@ namespace SAM.Core.Multitasker
         {
             get
             {
+                if(setting == null)
+                {
+                    setting = Load();
+                }
+
                 return setting;
             }
         }
